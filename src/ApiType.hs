348ab205d03cee7b45367b6156d2d621ed31ccc7
scowan1995 <scowan@tcd.ie>
2016-11-24 20:47:10 +0000

    just added more stuff from the tutorial

--- src/ApiType.hs
+++ src/ApiType.hs
@@ -19,6 +19,18 @@ type UserAPI3 = "users" :> "list-all" :> "now" :> Get '[JSON] [User]
               -- describes an endpoint reachable at:
               -- /users/list-all/now
 
+--an endpoint will alwasy end with a Verb combinator
+type UserAPI4 = "users" :> Get '[JSON] [User]
+           :<|> "admins" :> Get '[JSON] [User]
+
+type UserAPI5 = "user" :> Capture "userid" Integer :> Get '[JSON] User
+ -- equivalent to 'GET /user/:userid'
+-- except that we explicitly say that "userid" must be an integer
+-- bascially Capture (urlsegment name) (url segment type)
+
+--here [*] means a list of types
+data Verb method (statusCode :: Nat) (contentType :: [*]) a
+type Get = Verb 'GET 200
 data SortBy = Age | Name
 
 data User = User {
